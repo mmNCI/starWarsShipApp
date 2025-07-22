34d51be41cb1715edf61eb8ab07867b89fe5d54a
Mark MacGowan <130069175+MarkMacGowan@users.noreply.github.com>
2025-07-22 15:09:51 +0100

    feat: add continuous delivery

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+
+sudo npm install -g pm2
+
+pm2 stop starWarsShipApp
+
+cd starWarsShipApp
+
+npm install
+
+pm2 start ./bin/www --name starWarsShipApp
