e007b36a8ebb0985585e94f61b0cf971c0204829
Mark MacGowan <130069175+MarkMacGowan@users.noreply.github.com>
2025-07-23 16:45:53 +0100

    fix: remove possible duplicate

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 const shipsRouter = require('./routes/ships');
 
-app.use(express.static('public')); // serves index.html
+const app = express();
+
 app.use(express.json());
+//app.use(express.static('public')); // serves index.html
+
 
 
 
