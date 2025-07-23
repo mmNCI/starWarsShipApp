3441c50768fe8998dae2c9e30c044c11afd2c7b1
Mark MacGowan <130069175+MarkMacGowan@users.noreply.github.com>
2025-07-23 21:08:47 +0100

    fix: add db reference to integration test, and afterAll

--- test/integration/ships.test.js
+++ test/integration/ships.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../../app');
+const db = require('../../db');
 
 describe('GET /ships', () => {
     it('Aim to return list of ships', async () => {
@@ -7,5 +8,8 @@ describe('GET /ships', () => {
         expect(res.statusCode).toBe(200);
         expect(Array.isArray(res.body)).toBe(true);
     });
+});
+afterAll(async () => {
+    await db.close();
 });
 
