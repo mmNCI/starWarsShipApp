0169bfe5944e853fd2e12a1b97d00c5c170a2b7a
Mark MacGowan <130069175+MarkMacGowan@users.noreply.github.com>
2025-07-23 20:58:24 +0100

    feat: install jest for integration testing

--- test/integration/ships.test.js
+++ test/integration/ships.test.js
@@ -1,11 +1,11 @@
 const request = require('supertest');
 const app = require('../../app');
+
 describe('GET /ships', () => {
     it('Aim to return list of ships', async () => {
         const res = await request(app).get('/ships');
         expect(res.statusCode).toBe(200);
         expect(Array.isArray(res.body)).toBe(true);
-
     });
 });
 
